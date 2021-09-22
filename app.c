93e29a15010edc60c85e9d410c37f50e8b7a5a32
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-09-21 22:04:38 -0500

    Se agrega funcionalidad al  proyecto global

--- app.c
+++ app.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 //proyecto global
+#define PRECIO_HORA_EXTRA 60
+
+int anioActual =2021;
+
 
 int main(){
     int edad;
+    int annioNacimiento;
+    int horasExtrasTrabajadas;
+    int haRealizadoHorasExtras;//actua como boolean
     float sueldo;
+    float totalHorasExtraPagar; 
     char departamento;
 
     printf("¿Qué edad tiene?: \n");
@@ -15,6 +23,13 @@ int main(){
 
     printf("Departamento al que pertenece? \n");
     scanf(" %c", &departamento);
+
+    printf("Horas extras trabajas en este anio? \n");
+    scanf("%d", &horasExtrasTrabajadas);
+
+    annioNacimiento = anioActual -edad;
+    totalHorasExtraPagar =horasExtrasTrabajadas*PRECIO_HORA_EXTRA;
+    haRealizadoHorasExtras = totalHorasExtraPagar;
 
     return 0;
 }
