464b3635b7a1f4cb0c6ebd15678287f33ee2f01e
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-10-30 00:36:37 -0500

    Manipulación de  funciones que devuelven cadenas o char*

--- 2_C_basico_II/punteros_a_caracter/main.c
+++ 2_C_basico_II/punteros_a_caracter/main.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 // prototipos de las funciones
@@ -7,21 +9,53 @@ void recibePuntero(char *); // recibe un char*
 Nos ayuda a movernos por la cadena de una mejor forma, no es un recorte, es un avance de posición.
 */
 void aritmeticaDirecciones();
-char* obtenerSaludo();
-
-
-
+char *obtenerSaludo();
+char *substring(char *s, int desde, int hasta);
+char *rtrim(char *s);
 
 int main()
 {
-    char saludo[] = obtenerSaludo();
+    char *x = "Esto es una cadena      ";
+    printf("[%s]\n", rtrim(x));
     return 0;
 }
 
-char* obtenerSaludo() 
+char *substring(char *s, int desde, int hasta)
 {
- char a[] = "Hola, Mundo"; return a; // esto es incorrecto porque la variable a será destruida fuera de la función obetener saludo
-} 
+    int n = hasta - desde;
+    char *ret = (char *)malloc(n + 1);
+    int j = 0;
+    for (int i = desde; i < hasta; i++)
+    {
+        ret[j] = s[i];
+        j = j + 1;
+    }
+    ret[j] = '\0';
+    return ret;
+}
+
+char *rtrim(char *s)
+{
+    int i = strlen(s) - 1;
+    while (s[i] == ' ')
+    {
+        i = i - 1;
+    }
+    return substring(s, 0, i + 1);
+}
+
+char *obtenerSaludo()
+{
+    // cadena local
+    char a[] = "Hola, Mundo";
+    // longitud de la cadena que vamos a retornar
+    int n = strlen(a);
+    // array de n+1 caracteres generado dinamicamente
+    char *r = (char *)malloc(n + 1);
+    // asigna la cadena al array gestionado dinamicamente
+    strcpy(r, a);
+    return r;
+}
 
 void aritmeticaDirecciones()
 {
@@ -31,7 +65,6 @@ void aritmeticaDirecciones()
     printf("[%s]\n", a + 5);  // imprime: [es una cadena]
     printf("[%s]\n", a + 9);  // imprime: [na cadena]
     printf("[%s]\n", a + 12); // imprime: [cadena]
-
 }
 
 void recibeArray(char x[])
